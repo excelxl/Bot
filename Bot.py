41363df502eedda3ddf8d8d783f53528e04da6d7
excelxl <50279387+excelxl@users.noreply.github.com>
2023-08-05 23:04:02 +0700

    more coding

--- Bot.py
+++ Bot.py
@@ -6,6 +6,8 @@ from time import sleep
 import requests
 from Funcs import *
 from datetime import datetime
+from threading import Thread
+from datetime import datetime, date
 
 with open(r"C:\Users\white\Documents\Bot\config.json") as json_file:
     data = json.load(json_file)
@@ -23,11 +25,47 @@ client = tweepy.Client(
 )
 
 
+class retweet_follow(Thread):
+    def run(self):
+        while True:
+            print("Getting tweets from following people")
+            str = json.loads(client.get_home_timeline())
+            arr = []
+            for set in str:
+                arr.append([set["text"], set["id"]])
+            for i in arr:
+                print("Checking if their tweets meets the requirements")
+                if check(i["text"], criteria) == True:
+                    print("Retweet")
+                    client.retweet(i["id"])
+            sleep(60)
+
+
+class retweet_search(Thread):
+    def run(self):
+        while True:
+            now = datetime.utcnow() - datetime.timedelta(minutes=5)
+            format = "%Y-%m-%dT%H:%M:%SZ"
+            end = now.strftime(format)
+            print("Searching for tweets all over Twitter")
+            str = json.loads(
+                client.search_recent_tweets(query=query_params, start_time=str(end))
+            )
+            arr = []
+            for set in str.data:
+                arr.append([set.text, set.id])
+            for i in arr:
+                print("Checking tweets to see if it meets the requirement for retweet")
+                if check(i[0], criteria) == True:
+                    print("Retweet")
+                    client.retweet(i[1])
+            sleep(900)
+
+
 def check(input, criteria):
     count = 0
     arr = input.split(" ")
-    f = open("input.txt", "r")
-    inp = f.read()
+    inp = criteria.split(" ")
     for i in arr:
         if i in inp:
             count += 1
@@ -36,42 +74,14 @@ def check(input, criteria):
     return False
 
 
-def retweet_follow():
-    print()
-
-
-def retweet_search():
-    print()
-
-
 def main():
-    while True:
-        now = datetime.now()
-        time = now.strftime("%H")
-        if time == sleep:
-            sleep(60 * 120)
-        print("Searching for tweets all over Twitter")
-        str = json.loads(
-            client.search_recent_tweets(query=query_params, start_time="12:00:01")
-        )
-        arr = []
-        for set in str:
-            arr.append([set["text"], set["id"]])
-        for i in arr:
-            print("Checking tweets to see if it meets the requirement for retweet")
-            if check(i["text"], criteria) == True:
-                print("Retweet")
-                client.retweet(i["id"])
-        print("Getting tweets from following people")
-        str = json.loads(client.get_home_timeline())
-        for set in str:
-            arr.append([set["text"], set["id"]])
-        for i in arr:
-            print("Checking if their tweets meets the requirements")
-            if check(i["text"], criteria) == True:
-                print("Retweet")
-                client.retweet(i["id"])
-        sleep(time)
+    """while True:
+
+
+    sleep(time)"""
+    retweet_follow().start()
+    retweet_search().start()
+    sleep(10)
 
 
 if __name__ == "__main__":
