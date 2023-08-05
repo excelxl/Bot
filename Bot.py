e0c70d8b88d9c9a49883c83a3f3a9e08e5e5641b
excelxl <50279387+excelxl@users.noreply.github.com>
2023-08-05 22:21:37 +0700

    Update

--- Bot.py
+++ Bot.py
@@ -5,6 +5,22 @@ import json
 from time import sleep
 import requests
 from Funcs import *
+from datetime import datetime
+
+with open(r"C:\Users\white\Documents\Bot\config.json") as json_file:
+    data = json.load(json_file)
+api_key = data["API_KEY"]
+api_secret = data["API_SECRET"]
+bearer_token = data["BEARER_TOKEN"]
+access_token = data["ACCESS_TOKEN"]
+access_token_secret = data["ACCESS_TOKEN_SECRET"]
+query_params = data["QUERY"]
+criteria = data["CRITERIA"]
+time = int(data["TIME"])
+sleep = data["SLEEP"]
+client = tweepy.Client(
+    bearer_token, api_key, api_secret, access_token, access_token_secret
+)
 
 
 def check(input, criteria):
@@ -19,32 +35,25 @@ def check(input, criteria):
         return True
     return False
 
+
+def retweet_follow():
+    print()
+
+
+def retweet_search():
+    print()
+
+
 def main():
-    with open(r"C:\Users\white\Documents\Bot\config.json") as json_file:
-        data = json.load(json_file)
-    api_key = data["API_KEY"]
-    api_secret = data["API_SECRET"]
-
-    bearer_token = data["BEARER_TOKEN"]
-
-    access_token = data["ACCESS_TOKEN"]
-    access_token_secret = data["ACCESS_TOKEN_SECRET"]
-
-    query_params = data["QUERY"]
-    criteria = data["CRITERIA"]
-    time=int(data["TIME"])
-    client = tweepy.Client(
-        bearer_token, api_key, api_secret, access_token, access_token_secret
-    )
-
-    auth = tweepy.OAuth1UserHandler(
-        api_key, api_secret, access_token, access_token_secret
-    )
-
-    api = tweepy.API(auth)
     while True:
+        now = datetime.now()
+        time = now.strftime("%H")
+        if time == sleep:
+            sleep(60 * 120)
         print("Searching for tweets all over Twitter")
-        str = json.loads(client.search_recent_tweets(query=query_params,start_time="12:00:01"))
+        str = json.loads(
+            client.search_recent_tweets(query=query_params, start_time="12:00:01")
+        )
         arr = []
         for set in str:
             arr.append([set["text"], set["id"]])
@@ -54,7 +63,7 @@ def main():
                 print("Retweet")
                 client.retweet(i["id"])
         print("Getting tweets from following people")
-        str=json.loads(client.get_home_timeline())
+        str = json.loads(client.get_home_timeline())
         for set in str:
             arr.append([set["text"], set["id"]])
         for i in arr:
